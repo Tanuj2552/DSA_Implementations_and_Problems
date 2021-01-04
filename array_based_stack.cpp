af99b6688345ad0f8b210f480d1194cc451b68a7
Tanuj <tanujtvs2552@gmail.com>
2021-01-04 13:27:08 +0530

    Create array_based_stack.cpp

---
+++ array_based_stack.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Stack{
+  public:
+    int arr[1000];
+    int top;
+
+  Stack(){
+    top = -1;
+  }
+
+  void push(int k){
+    top++;
+    arr[top] = k;
+  }
+
+  int pop(){
+    if(head == NULL){cout << "pop not possible" << endl; return;}
+    if(top==-1){cout << "Empty Stack" << endl;}
+    int a = arr[top];
+    top--;
+    return a;
+  }
+
+  int up(){
+    return arr[top];
+  }
+
+  void print(){
+    for(int i=0; i<=top; i++){
+      cout <<"Elements are: " << endl;
+      cout << arr[i] << endl;
+    }
+  }
+
+};
+
+int main(){
+  Stack s;
+  s.push(2);
+  s.push(3);
+  int k = s.pop(); cout << k << endl;
+  k = s.up(); cout << k << endl;
+  s.print();
+
+}
