0b942664e332ee983d765503e5023ebcf7cc8c0f
Tanuj <tanujtvs2552@gmail.com>
2021-01-08 19:18:02 +0530

    Update Singly_linked_list.cpp

--- Singly_linked_list.cpp
+++ Singly_linked_list.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 /* a Node of the doubly linked list */
@@ -43,6 +44,16 @@ void lens(Node** h){
   cout << "len of linked list is " << l << endl;
 }
 
+int get_len(Node** h){
+   Node* temp = *h;
+  int l = 0;
+  while(temp){
+    l++;
+    temp = temp->next;
+  }
+  return l;
+}
+
 void delete_element(Node** h, int k){
   Node* temp = *h;
   if(temp->data == k){*h = temp->next;return ;}
@@ -61,11 +72,40 @@ void find_pos(Node** h, int k){
   Node* temp = *h;
   int pos = 0;
   while(temp){
-    if(temp->data == k){cout << "the element " << k << " is at position " << pos;return;}
+    if(temp->data == k){cout << "the element " << k << " is at position " << pos << endl;return;}
     temp = temp->next;
     pos++;
   }
-  cout << "element not found!!!" ;
+  cout << "element not found!!! " << endl;
+}
+
+void swap_values_of_given_indices (Node** h, int p, int q){
+  if(p>=get_len(h) || q>=get_len(h)){
+    cout << "indices out of range!! " << endl; return;
+  }
+  else if(p==q){cout << "no change !!" << endl; return;}
+  int c = 0;
+  int m = min(p,q);
+  int n = p+q-m;
+  Node* temp = *h;
+  int a,b;
+  Node* temp_1=NULL;
+  Node* temp_2=NULL;
+  while(temp){
+    if(m==c){
+      temp_1 = temp;
+      a = temp->data;
+    }
+    else if(n==c){
+      temp_2 = temp;
+      b = temp->data;
+    }
+    temp = temp->next;
+    c++;
+  }
+  temp_1->data = b;
+  temp_2->data = a;
+
 }
 
 int main(){
@@ -91,6 +131,12 @@ int main(){
   cout << "enter the element to be checked and find its pos : ";
   cin >> ch;
   find_pos(&head, ch);
-  
+
+  int m_swap,n_swap;
+  cout << "Enter the indices of the linked lists to be swapped " << endl;
+  cin >> m_swap >> n_swap;
+  swap_values_of_given_indices(&head, m_swap,n_swap);
+
+  print_list(&head);
   return 0;
 }
