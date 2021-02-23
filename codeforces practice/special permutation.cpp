b2ea16e9b268feab6a4f7d3f2382af1551c50404
Tanuj <tanujtvs2552@gmail.com>
2021-02-23 22:38:18 +0530

    The link for the question is
    https://codeforces.com/contest/1454/problem/A

---
+++ codeforces practice/special permutation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        for(int i = 2;i<=n;i++){cout << i << ' ' ;}
+            cout << 1 << endl;
+ 
+        }
+ 
+}
