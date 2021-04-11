d30560805ca04faf05abbc79e740c824d81957a3
Tanuj <tanujtvs2552@gmail.com>
2021-04-11 22:28:22 +0530

    The link for the question is
    https://www.hackerrank.com/challenges/validating-postalcode/problem

---
+++ hacker_rank_practise/validating postal codes.py
@@ -0,0 +1,3 @@
+regex_integer_in_range = r"^[1-9]\d{5}?$"    # Do not delete 'r'.
+regex_alternating_repetitive_digit_pair = r"(0(?=\d0))|(1(?=\d1))|(2(?=\d2))|(3(?=\d3))|(4(?=\d4))|(5(?=\d5))|(6(?=\d6))|(7(?=\d7))|(8(?=\d8))|(9(?=\d9))"
+
